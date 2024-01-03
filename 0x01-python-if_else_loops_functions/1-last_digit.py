c1cd435117040442a0c25d29e9cfeffb4dcb9ed0
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-01-03 16:07:20 +0100

    my_project

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     number *= -1
-    nb = number % 10 
+    nb = number % 10
     print(f"Last digit of -{number} is -{nb} and is less than 6 and not 0")
 else:
     nb = number % 10
