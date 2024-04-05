55f6521361db4c8aa4435a913d3009c82fc68593
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-05 22:55:10 +0000

    i finish

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,10 +2,10 @@
 
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-	"""Print all the results of the basic operations."""
-	a = 10
-	b = 5
-	print("{} + {} = {}".format(a, b, add(a, b)))
-	print("{} - {} = {}".format(a, b, sub(a, b)))
-	print("{} * {} = {}".format(a, b, mul(a, b)))
-	print("{} / {} = {}".format(a, b, div(a, b)))
+    """Print all the results of the basic operations."""
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
