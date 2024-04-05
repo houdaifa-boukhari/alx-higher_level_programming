0dc34ce312d3a2d63d2803580d729076becaa80e
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-05 14:45:41 +0000

    i finish

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-	if c >= 'A' and c <= 'Z':
-		return True
-	else:
-		return False
+    if c >= 'A' and c <= 'Z':
+        return True
+    else:
+        return False
