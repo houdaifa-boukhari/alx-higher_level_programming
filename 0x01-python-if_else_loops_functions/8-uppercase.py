8b4ea951aeb0b2ac293c0f15a1150b0a0b5f3004
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-05 15:20:04 +0000

    i finish

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def islower(c):
-    if c >= 'A' and c <= 'Z':
+    if not c:
+        raise ValueError("Input cannot be an empty string")
+    if c >= 'a' and c <= 'z':
         return True
     else:
         return False
+
+def uppercase(str):
+    i = 0
+    while str[i]:
+        if islower(str[i]):
+            print(str[i])
+        else:
+            print(str[i])
+        i += 1
+                  
