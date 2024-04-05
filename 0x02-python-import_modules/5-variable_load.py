8796f4c04f236a9b5b2cb8c36d344515d898693c
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-05 23:18:54 +0000

    i finish

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	"""Print the value of the variable a from the module variable_load_5"""
-	from variable_load_5 import a
-	print(a)
+    """Print the value of the variable a from the module variable_load_5"""
+    from variable_load_5 import a
+    print(a)
