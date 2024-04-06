d92fcd6d0a630fca9ae22afb1de2f1a56bf76a97
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-06 18:36:36 +0000

    good job

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,28 +1,28 @@
 #include "lists.h"
 #include <stdlib.h>
 
-// size_t ft_lstsize(listint_t *head);
+size_t ft_lstsize(listint_t *head);
 int is_palindrome(listint_t **head);
 
 /**
  * ft_lstsize - returns the number of elements in a linked list
  * @head: pointer to the head of the list
  * Return: number of elements in the list
-//  */
+*/
 
-// size_t ft_lstsize(listint_t *head)
-// {
-// 	size_t count = 0;
+size_t ft_lstsize(listint_t *head)
+{
+	size_t count = 0;
 
-// 	// if (!head)
-// 		return (0);
-// 	while (head)
-// 	{
-// 		count++;
-// 		head = head->next;
-// 	}
-// 	return (count);
-// }
+	if (!head)
+		return (0);
+	while (head)
+	{
+		count++;
+		head = head->next;
+	}
+	return (count);
+}
 
 /**
  * is_palindrome - checks if a linked list is a palindrome
@@ -33,25 +33,26 @@ int is_palindrome(listint_t **head);
 
 int is_palindrome(listint_t **head)
 {
-	// size_t count = ft_lstsize(*head);
-	// int *my_list;
-	// listint_t *current = *head;
+	size_t count = ft_lstsize(*head);
+	int *my_list;
+	size_t i = 0;
+	listint_t *current = *head;
 
-	// if (!head || !*head)
-	// 	return (0);
-	// my_list = (int *)malloc(sizeof(int) * count);
-	// if (!my_list)
-		// return (0);
-	// for (size_t i = count - 1; current; i--, current = current->next)
-		// my_list[i] = current->n;
-	// current = *head;
-	// for (int i = 0; current; i++, current = current->next)
-	// {
-		// if (current->n != my_list[i])
-		// {
-			// free(my_list);
-			// return (0);
-		// }
-	// }
+	if (!head || !*head)
+		return (0);
+	my_list = (int *)malloc(sizeof(int) * count);
+	if (!my_list)
+		return (0);
+	for (i = count - 1; current; i--, current = current->next)
+		my_list[i] = current->n;
+	current = *head;
+	for (i = 0; current; i++, current = current->next)
+	{
+		if (current->n != my_list[i])
+		{
+			free(my_list);
+			return (0);
+		}
+	}
 	return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -22,6 +22,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-// size_t ft_lstsize(listint_t *head);
+size_t ft_lstsize(listint_t *head);
 
 #endif /* LISTS_H */
