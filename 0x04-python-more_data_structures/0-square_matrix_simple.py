50d9ead7de5b0d3103c2888c97258983986f4470
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-06 20:28:08 +0000

    i finish

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+    for i in range(len(matrix)):
+        for j in (range(len(matrix[i]))):
+            matrix[i][j] **= 2
+    return new_matrix
