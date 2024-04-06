82af1fd8435023ad5c32901f39a616a732bd8388
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-06 17:45:30 +0000

    i finish

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,19 +7,19 @@
  * Return: number of elements in the list
  */
 
-// int ft_lstsize(listint_t *head)
-// {
-// 	int count = 0;
+int ft_lstsize(listint_t *head)
+{
+	int count = 0;
 
-// 	if (!head)
-// 		return (0);
-// 	while (head)
-// 	{
-// 		count++;
-// 		head = head->next;
-// 	}
-// 	return (count);
-// }
+	if (!head)
+		return (0);
+	while (head)
+	{
+		count++;
+		head = head->next;
+	}
+	return (count);
+}
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -29,26 +29,26 @@
 
 int is_palindrome(listint_t **head)
 {
-	// int count = ft_lstsize(*head);
-	// int *my_list;
-	// listint_t *current = *head;
+	int count = ft_lstsize(*head);
+	int *my_list;
+	listint_t *current = *head;
 
-	// if (!head || !*head)
-	// 	return (0);
-	// my_list = (int *)malloc(sizeof(int) * count);
-	// if (!my_list)
-	// 	return (0);
-	// for (int i = count - 1; current && i >= 0; i--, current = current->next)
-	// 	my_list[i] = current->n;
-	// current = *head;
-	// for (int i = 0; current; i++, current = current->next)
-	// {
-	// 	if (current->n != my_list[i])
-	// 	{
-	// 		free(my_list);
-	// 		return (0);
-	// 	}
-	// }
-	// free(my_list);
+	if (!head || !*head)
+		return (0);
+	my_list = (int *)malloc(sizeof(int) * count);
+	if (!my_list)
+		return (0);
+	for (int i = count - 1; current && i >= 0; i--, current = current->next)
+		my_list[i] = current->n;
+	current = *head;
+	for (int i = 0; current; i++, current = current->next)
+	{
+		if (current->n != my_list[i])
+		{
+			free(my_list);
+			return (0);
+		}
+	}
+	free(my_list);
 	return (1);
 }
