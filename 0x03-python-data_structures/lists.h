8461d1f9633f76be0643c529f072e07c280b4c97
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-06 17:29:49 +0000

    i finish

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -16,7 +16,7 @@ typedef struct listint_s
 } listint_t;
 
 # include <stdio.h>
-# include <stdlib.h>
+
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
