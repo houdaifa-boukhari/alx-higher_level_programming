21a340b181b4226e91bc116931cbae7438b43bb3
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-06 21:00:00 +0000

    i finish

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
 def is_unique(new_list, nb):
-	if not new_list:
-		return True
-	for i in range(len(new_list)):
-		if new_list[i] == nb:
-			return False
-	return True
-	
+    if not new_list:
+        return True
+    for i in range(len(new_list)):
+        if new_list[i] == nb:
+            return False
+    return True
+    
 def uniq_add(my_list=[]):
-	new_list = []
-	sum = 0
-	for i in range(len(my_list)):
-		if is_unique(new_list, my_list[i]) == True:
-			new_list.append(my_list[i])
-	for i in range(len(new_list)):
-		sum += new_list[i]
-	return sum
+    new_list = []
+    sum = 0
+    for i in range(len(my_list)):
+        if is_unique(new_list, my_list[i]) == True:
+            new_list.append(my_list[i])
+    for i in range(len(new_list)):
+        sum += new_list[i]
+    return sum
