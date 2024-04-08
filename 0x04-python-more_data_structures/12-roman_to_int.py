0a7e8960850dd7915963d0fa76aecfeabf7248eb
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-08 23:33:40 +0000

    i finish

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python3
 
+def search_value(str):
+	a_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+	for key, value in a_dictionary.items():
+		if key == str:
+			return value
+	return None
+	
 def roman_to_int(roman_string):
 	if not roman_string or type(roman_string) is not str:
 		return 0
-	a_dictionaire = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-	for key, value in a_dictionaire.items():
-		roman_string = roman_string.replace(key, str(value) + ' ')
-	
+	sum = 0
+	for i in range(len(roman_string)):
+		if i > 0 and search_value(roman_string[i]) > search_value(roman_string[i - 1]):
+			sum -= search_value(roman_string[i - 1]) * 2
+		sum += search_value(roman_string[i])
+	return sum
+
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
