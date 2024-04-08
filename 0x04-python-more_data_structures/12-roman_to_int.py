0b869e34c4e34dc05a4e7e3ec50b2caf97fbc171
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-08 23:51:57 +0000

    i finish

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def get_value(str):
-    dic = {'I': 1, 'V': 5,'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+def get_val(str):
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for key, value in dic.items():
         if key == str:
             return value
@@ -13,7 +13,7 @@ def roman_to_int(roman_string):
         return 0
     sum = 0
     for i in range(len(roman_string)):
-        if i > 0 and get_value(roman_string[i]) > get_value(roman_string[i - 1]):
-            sum -= get_value(roman_string[i - 1]) * 2
-        sum += get_value(roman_string[i])
+        if i > 0 and get_val(roman_string[i]) > get_val(roman_string[i - 1]):
+            sum -= get_val(roman_string[i - 1]) * 2
+        sum += get_val(roman_string[i])
     return sum
