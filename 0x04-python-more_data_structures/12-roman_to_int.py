acf94d490d5b8d55fb67f0eedf4b1bcc63c2696a
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-08 23:21:37 +0000

    i finish

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,4 +3,7 @@
 def roman_to_int(roman_string):
 	if not roman_string or type(roman_string) is not str:
 		return 0
+	a_dictionaire = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+	for key, value in a_dictionaire.items():
+		roman_string = roman_string.replace(key, str(value) + ' ')
 	
