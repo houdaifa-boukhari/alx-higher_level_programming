dd58ec79d06594bb68427188c465b81f5a020790
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-09 02:32:07 +0000

    i finish

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,14 +39,14 @@ class Square:
 
     def my_print(self):
         tmp = self.__position[0] * self.__size
-        if self.__position[1] > 0:
+        if self.__position[1] > 0 and tmp != 0:
             tmp /= self.__position[0]
         if self.__size == 0:
             print()
         else:
             for i in range(self.__size):
-                 if tmp > 0:
-                     for i in range(self.__position[0]):
+                if tmp > 0:
+                    for i in range(self.__position[0]):
                         print(" ", end="")
-                     tmp -= 1
-                 print("#" * self.__size)
+                    tmp -= 1
+                print("#" * self.__size)
