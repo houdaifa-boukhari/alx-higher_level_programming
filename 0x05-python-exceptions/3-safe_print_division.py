00ebea455f41c9b8c1ca7d8b4a3995971832e27d
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-11 23:47:08 +0000

    push

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, ValueError):
         return None
     finally:
-        print("{}".format(sum))
+        print("Inside result: {}".format(sum))
         return sum
