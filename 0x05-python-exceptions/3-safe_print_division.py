8c233ee3bd8aa17cf327e7e78e6cf360b393c2df
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-11 23:50:05 +0000

    push

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,10 +2,19 @@
 
 def safe_print_division(a, b):
     sum = 0
+    flage = 0
     try:
         sum = a / b
     except (ZeroDivisionError, ValueError):
-        return None
+        flage = 1
     finally:
+        if flage = 1:
+            print("Inside result: None")
+            return "None"
         print("Inside result: {}".format(sum))
         return sum
+
+a = 10
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
