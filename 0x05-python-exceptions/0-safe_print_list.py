52055c285e19298114df4ec4e596f50aea21dc54
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-11 23:09:02 +0000

    push

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    if x == 0:
+        return 0
     try:
         for i  in range(x):
             print(my_list[i], end="")
@@ -9,3 +11,8 @@ def safe_print_list(my_list=[], x=0):
         return (i)
     print()
     return (i + 1)
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 0)
+print("nb_print: {:d}".format(nb_print))
