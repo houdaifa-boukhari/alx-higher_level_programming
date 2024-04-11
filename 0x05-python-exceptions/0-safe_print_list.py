4e689b3dc2cb7876cbc5ebbf17ee2648066942d6
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-11 23:12:35 +0000

    push

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i  in range(x):
             print(my_list[i], end="")
-    except :
+    except:
         print()
         return (i)
     print()
