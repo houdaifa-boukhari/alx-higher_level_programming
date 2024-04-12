09eeb486b71bea152fb34c9d5b433d144ec0c41f
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-12 00:08:13 +0000

    push

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,3 +20,8 @@ def list_division(my_list_1, my_list_2, list_length):
     except IndexError:
         print("out of range")
     return new_list
+
+my_l_1 = [10, 0, 4]
+my_l_2 = [0, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
