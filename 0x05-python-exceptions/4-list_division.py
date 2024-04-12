839b59fd0a5b055cb2686787ae946769a549a972
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-12 00:05:45 +0000

    push

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def safe_division(a, b):
+    sum = 0
+    try:
+        sum = a / b
+    except (ZeroDivisionError):
+        print("division by 0")
+    except (TypeError, ValueError):
+        print("wrong type")
+    finally:
+        return sum
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    try:
+        for i in range(list_length):
+            new_list.append(safe_division(my_list_1[i], my_list_2[i]))
+    except IndexError:
+        print("out of range")
+    return new_list
