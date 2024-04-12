c6849d699a7d20c3db0ce7c1315d2374635198e0
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-12 00:30:19 +0000

    push

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        print("{:d}".format())
+        print("{:d}".format(value))
     except (TypeError, ValueError):
         return False
     return True
