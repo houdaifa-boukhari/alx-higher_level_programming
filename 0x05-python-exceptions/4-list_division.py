f08ba6fc9e40667f7fa89434b6d9f9777c72b4bd
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-12 00:19:00 +0000

    push

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,10 +14,10 @@ def safe_division(a, b):
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    try:
-        for i in range(list_length):
+    for i in range(list_length):
+        if i < min(len(my_list_1), len(my_list_2)):
             new_list.append(safe_division(my_list_1[i], my_list_2[i]))
-    except IndexError:
-        print("out of range")
+        else:
+            print("out of range")
+            new_list.append(0)
     return new_list
-
