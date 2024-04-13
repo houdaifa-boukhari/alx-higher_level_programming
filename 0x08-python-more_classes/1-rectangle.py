50410800ff056f229653774a581c125790255ff9
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-04-13 20:53:32 +0000

    push

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,3 +33,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+
+my_rectangle = Rectangle(-2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = -10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
